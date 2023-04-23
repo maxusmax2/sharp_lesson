d918618c44186a8efbcb17e0f498d11e69d71540
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-04-23 19:58:44 +0300

    Первое задание на ООП

---
+++ lesson1OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+Transport[] transports = new Transport[] { new Car(),new Airplane(),new Boat()};
+
+foreach(Transport transport in transports) 
+{
+    transport.Move();
+}
+
+public abstract class Transport 
+{
+    public abstract void Move();
+}
+
+class Car : Transport
+{
+    public override void Move()
+    {
+        WriteLine("Ехать");
+    }
+}
+
+class Airplane : Transport
+{
+    public override void Move()
+    {
+        WriteLine("Летать");
+    }
+}
+
+class Boat : Transport
+{
+    public override void Move()
+    {
+        WriteLine("Плыть");
+    }
+}
