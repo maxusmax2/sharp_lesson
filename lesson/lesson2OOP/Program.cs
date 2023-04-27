6bfcf338ebba641efa2a65b9e221be9135947161
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-04-27 20:53:15 +0300

    Переделано задание 2 : добавлены проверки на null, константы и убраны небезопасные сеттеры

--- lesson/lesson2OOP/Program.cs
+++ lesson/lesson2OOP/Program.cs
@@ -8,49 +8,72 @@ void SavePathToDB(string cargoId, NauticalMiles milesPath, Kilometer KmPath)
 }
 var id = "test";
 var kilometer1 = new Kilometer(100);
-var nauticalMiles = new NauticalMiles(40);
+NauticalMiles nauticalMiles = new NauticalMiles(40);
 var kilometer2 = new Kilometer(80);
 SavePathToDB(id, (NauticalMiles)kilometer1, (Kilometer)nauticalMiles + kilometer2);
 
 public class Kilometer
 {
-    public Double Value { get; set; }
+    private const Double _kilometerToMilesCoefficient = 1.852;
+    public  Double Value { get;}
     public Kilometer(Double value)
     {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
         Value = value;
     }
     public static Kilometer operator +(Kilometer kilometer, NauticalMiles miles)
     {
-        return new Kilometer(miles.Value * 1.852 + kilometer.Value);
+        if (miles == null) throw new ArgumentNullException(nameof(miles));
+        if (kilometer == null) throw new ArgumentException(nameof(kilometer));
+        return new Kilometer(miles.Value * _kilometerToMilesCoefficient + kilometer.Value);
     }
-    public static explicit operator NauticalMiles(Kilometer kilo) => new NauticalMiles(kilo.Value * 0.539957);
+    public static explicit operator NauticalMiles(Kilometer kilo)
+    {
+        if (kilo == null) throw new ArgumentException(nameof(kilo)); 
+        return new NauticalMiles(kilo.Value * _kilometerToMilesCoefficient);
+    }
     public static Kilometer operator *(Kilometer kilometer, Double val)
     {
+        if (kilometer == null) throw new ArgumentNullException(nameof(kilometer));
+        if (val < 0) throw new ArgumentOutOfRangeException(nameof(val));
         return new Kilometer(kilometer.Value * val);
     }
     public static Kilometer operator +(Kilometer kilo1, Kilometer kilo2)
-    {
+    {   
+        if (kilo1 == null) throw new ArgumentNullException(nameof(kilo1));
         return new Kilometer(kilo1.Value + kilo2.Value);
     }
 }
 public class NauticalMiles
 {
-    public Double Value { get; set; }
-    public NauticalMiles(Double value) 
+    private const Double _milesToKilometerCoefficient = 0.539957;
+    public Double Value { get;}
+    public NauticalMiles(Double value)
     {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
         Value = value;
     }
-    public static explicit operator Kilometer(NauticalMiles miles) => new Kilometer(miles.Value * 0.539957);
+    public static explicit operator Kilometer(NauticalMiles miles) 
+    {
+        if (miles == null) throw new ArgumentNullException(nameof(miles));
+        return new Kilometer(miles.Value * _milesToKilometerCoefficient);
+    } 
     public static NauticalMiles operator +(NauticalMiles miles, Kilometer kilometer) 
     {
-        return new NauticalMiles(miles.Value + kilometer.Value * 0.539957 );
+        if (miles == null) throw new ArgumentNullException(nameof(miles));
+        if (kilometer == null) throw new ArgumentException(nameof(kilometer));
+        return new NauticalMiles(miles.Value + kilometer.Value *_milesToKilometerCoefficient);
     }
     public static NauticalMiles operator +(NauticalMiles miles1, NauticalMiles miles2)
     {
+        if (miles1 == null) throw new ArgumentNullException(nameof(miles1));
+        if (miles2 == null) throw new ArgumentNullException(nameof(miles2));
         return new NauticalMiles(miles1.Value + miles2.Value);
     }
     public static NauticalMiles operator *(NauticalMiles miles, Double val)
     {
+        if (miles == null) throw new ArgumentNullException(nameof(miles));
+        if (val < 0) throw new ArgumentOutOfRangeException(nameof(val));
         return new NauticalMiles(miles.Value * val);
     }
 }
