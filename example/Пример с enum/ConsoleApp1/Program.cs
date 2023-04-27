8fb5f2e900a6b82571634a12ac894636a97882a8
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-04-27 21:06:25 +0300

    Добавлен пример с enum

---
+++ example/Пример с enum/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine((C.B).ToString("D"));//1
+Console.WriteLine((C.C2).ToString("D"));//2
+Console.WriteLine((C.C1).ToString("D"));//1
+Console.WriteLine((C.C3).ToString("D"));//2
+Console.WriteLine((C.C4).ToString("D"));//3
+
+enum C
+{
+    B = 1,
+    C2,
+    C1 = 1,
+    C3,
+    C4,
+};
+
