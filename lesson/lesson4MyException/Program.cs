c3ea54b99cb0726d04453267dfea0831f3006c7a
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-05-02 17:42:23 +0300

    4 задание с кастомным исключением

---
+++ lesson/lesson4MyException/Program.cs
@@ -0,0 +1,14 @@
+﻿[Serializable]
+class MyException : Exception 
+{
+    public string MyProperty { get; }
+    public MyException() { }
+    public MyException(string message) : base(message) { }
+    public MyException(string message, Exception innerException) : base(message, innerException) { }
+    public MyException(string message, string myProperty) 
+        : this(message) 
+    {
+        MyProperty = myProperty;
+    }
+
+}
