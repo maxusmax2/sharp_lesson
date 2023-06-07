9da7085223cae1eb5ae132388fcdf6ca8c0cd34d
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-06-07 17:34:22 +0300

    Добавлена функция отображения списка сессий пользователя

---
+++ lesson/MS_SQL_SSDT/SessionsUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[SessionsUser]
+(
+	@UserId int,
+	@PageNum int,
+	@RowOnPage int
+)
+RETURNS TABLE
+RETURN
+(
+	SELECT * FROM 
+	LOGINS WHERE UserId = @UserId 
+	ORDER BY Date 
+	OFFSET @PageNum ROWS
+	FETCH NEXT @RowOnPage ROWS ONLY
+)
+
